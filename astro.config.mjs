9e9ea201108bc8b2a4d81853fd0dd017afaca208
Okiki Ojo <okikio.dev@gmail.com>
2022-12-16 21:52:29 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -19,7 +19,7 @@ export default defineConfig({
     dist: new URL('./dist/', import.meta.url)
   }),
   experimental: {
-    prerender: true,
+    prerender: false,
     errorOverlay: true,
     contentCollections: false,
   },
