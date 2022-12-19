bf03b0d125adc8359361ae2dfbfa7a804a0dc583
Okiki <okikio.dev@gmail.com>
2022-12-19 17:19:06 +0000

    feat: support tweet unified_cards

--- src/utils.ts
+++ src/utils.ts
@@ -24,6 +24,13 @@ function getTweetDetails(res: object) {
 function getCardDetails(res: object) {
   // @ts-ignore
   const card_binding_values = res.card.binding_values;
+  if ("unified_card" in card_binding_values) {
+    // @ts-ignore
+    const json = JSON.parse(card_binding_values.unified_card.string_value as unknown as string) as Record<string, string>;
+    const media_entities = { mediaDetails: Object.values(json.media_entities) };
+    return getTweetDetails(media_entities);
+  }
+
   const thumbnail_image_original = card_binding_values?.thumbnail_image_original?.image_value.url;
   if (!("thumbnail_image_original" in card_binding_values)) return [];
 
@@ -43,10 +50,11 @@ export async function getMediaURL(url: string) {
 
   if (exec) {
     const id = exec.pathname.groups.id;
+    console.log(id)
     const url = `https://cdn.syndication.twimg.com/tweet-result?id=${id}&lang=en`;
     const res = await (await fetch(url)).json();
-    console.log({ url })
-    console.log(res)
+    // console.log({ url })
+    // console.log(res)
 
     let obj = res;
     let arr: ReturnType<typeof getTweetDetails> = [];
@@ -62,7 +70,7 @@ export async function getMediaURL(url: string) {
       obj = obj.quoted_tweet;
 
       if ("mediaDetails" in obj) {
-        arr = [...arr].concat( getTweetDetails(obj) );
+        arr = [...arr].concat(getTweetDetails(obj) );
       }
 
       if ("card" in obj) {
